4a3dd4c4b53e25f3a8f4653f7019e178d3516cb2
Sergio Casero Hernández <sergio.casero23@gmail.com>
2016-02-25 19:31:35 +0100

    Added PodcastService and Interactor class

---
+++ data/src/main/java/com/sergiocasero/podcasts/service/PodcastService.java
@@ -0,0 +1,8 @@
+package com.sergiocasero.podcasts.service;
+
+/**
+ * Created by sergiocasero on 25/2/16.
+ */
+public interface PodcastService {
+    public static final String ENDPOINT = "http://todotelegram.com/sport_podcast/";
+}
