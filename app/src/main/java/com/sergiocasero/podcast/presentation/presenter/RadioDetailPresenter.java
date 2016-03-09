4c23acbd767a1ce13357e7488653089ba63f54d0
Sergio Casero Hernández <sergio.casero23@gmail.com>
2016-03-09 21:28:25 +0100

    Added presenter! :)

---
+++ app/src/main/java/com/sergiocasero/podcast/presentation/presenter/RadioDetailPresenter.java
@@ -0,0 +1,38 @@
+package com.sergiocasero.podcast.presentation.presenter;
+
+import javax.inject.Inject;
+
+/**
+ * Created by sergiocasero on 9/3/16.
+ */
+public class RadioDetailPresenter extends Presenter<RadioDetailPresenter.RadioDetailView> {
+
+    @Inject
+    public RadioDetailPresenter() {
+    }
+
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    public interface RadioDetailView extends Presenter.View {
+
+    }
+}
--- app/src/main/java/com/sergiocasero/podcast/presentation/view/activity/RadioDetailActivity.java
+++ app/src/main/java/com/sergiocasero/podcast/presentation/view/activity/RadioDetailActivity.java
@@ -4,19 +4,30 @@ import android.os.Bundle;
 
 import com.sergiocasero.podcast.R;
 import com.sergiocasero.podcast.presentation.presenter.Presenter;
+import com.sergiocasero.podcast.presentation.presenter.RadioDetailPresenter;
 
 /**
  * Created by sergiocasero on 9/3/16.
  */
 public class RadioDetailActivity extends RootActivity {
 
-
+    RadioDetailPresenter radioDetailPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initializeInjector();
+        initializePresenter();
     }
 
+
+    private void initializeInjector() {
+
+    }
+
+    private void initializePresenter() {
+
+    }
     @Override
     public int getLayoutResourceId() {
         return R.layout.activity_radio_detail;
